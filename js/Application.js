cf375fd22d2ff29aa1ba07db902dffe5f996336b
Vlad <example@gmail.com>
2023-07-27 15:59:29 +0200

    AppCheckPoint2

--- js/Application.js
+++ js/Application.js
@@ -2,3 +2,10 @@
 
 let number0fFilms = prompt("Сколько фильмов вы уже посмотрели?");
 
+let personalMovieDB = {
+    count: number0fFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
