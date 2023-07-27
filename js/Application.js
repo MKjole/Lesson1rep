23d18e48a010fc01079723068ded5c7f96413e28
Vlad <example@gmail.com>
2023-07-27 16:05:49 +0200

    AppCheckPoint3

--- js/Application.js
+++ js/Application.js
@@ -9,3 +9,12 @@ let personalMovieDB = {
     genres: [],
     privat: false
 };
+
+let answer1 = prompt("Один из последних просмотренных фильмов?");
+let answer1_1 = prompt("На сколько оцените его?");
+let answer2 = prompt("Один из последних просмотренных фильмов?");
+let answer2_1 = prompt("На сколько оцените его?");
+movies: {
+    answer1: answer1_1;
+    answer2: answer2_1
+}
